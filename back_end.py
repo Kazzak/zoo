99309283952f5645e82e5d89a51bb3ee017e72eb
python <python@python.(none)>
2012-05-12 21:43:04 -0600

    semi consulta

--- back_end.py
+++ back_end.py
@@ -1,16 +1,43 @@
 from pyswip import *
 
+#lista para variables a unificar
+R = Variable()
+ED = Variable()
+G = Variable()
+N = Variable()
+EC = Variable()
+C = Variable()
+variables = [R,ED,G,N,EC,C]
+
 assertz = Functor("assertz", 1)
 animal = Functor("animal", 6)
 
-def insertar(raza,edad,genero,nombre,ecosistema,comida):
-	#se inserta el animal
-	call(assertz(animal(raza,edad,genero,nombre,ecosistema,comida)))
-	print("se inserto la vara")
-	
+
 	#para hacer la consulta del insert anterior
 	#X = Variable()
 	#q = Query(animal(raza,edad,genero,X,ecosistema,comida))
 	#while q.nextSolution():
 	#	print "animal:,", X.value
 	#q.closeQuery()
+
+def insertar(raza,edad,genero,nombre,ecosistema,comida):
+	#se inserta el animal
+	call(assertz(animal(raza,edad,genero,nombre,ecosistema,comida)))
+	print("se inserto la vara")
+	
+	
+def consultar(lista):
+	#['perro', '12', Variable(19), Variable(20), 'cartago', 'queso']
+	for i in range(0, len(lista)):
+		if lista[i] == "":
+			lista[i]= variables[i]
+	q = Query(animal(lista[0],lista[1],lista[2],lista[3],lista[4],lista[5]))
+	while q.nextSolution():
+		for i in range(0,len(variables)):
+			print(variables[i].value)
+	q.closeQuery()
+	
+	
+	
+	
+
